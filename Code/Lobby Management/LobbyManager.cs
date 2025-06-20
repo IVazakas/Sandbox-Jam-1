6df6695f9757fde5e9f32820146112f9199b78a7
Zach <53870574+Jadefire16@users.noreply.github.com>
2025-06-19 20:39:58 -0400

    Intro To UI
    To quote a once famous legend “My hope is that this code is so bad I'm never allowed to write UI code again.”
    
    Unfortunately I'm the only one able to write UI atm so here we are.
    
    Added:
    Temporary HUD and associated style file
    
    Removed:
    A bunch of template stuff that was awful and didn't actually work
    
    Kudos to Carson Kompon for his excellent tutorials, otherwise we'd be making a game without UI

---
+++ Code/Lobby Management/LobbyManager.cs
@@ -0,0 +1,36 @@
+using Sandbox;
+using Sandbox.Network;
+using Sandbox.UI;
+using Sandbox.Utility;
+using System.Threading.Tasks;
+
+
+public sealed class LobbyManager : Component
+{
+	[Property] public int MaxLobbyMembers { get; set; } = 8;
+
+	[RequireComponent, Property] public ScreenPanel Root { get; set; }
+
+	protected override void OnAwake()
+	{
+		Root = GameObject.GetComponent<ScreenPanel>();
+	}
+
+
+	public void HostLobby()
+	{
+		Log.Info( "Hosting Lobby..." );
+
+		var config = new LobbyConfig()
+		{
+			Hidden = true,
+			Name = "RepWarLobby.test",
+			MaxPlayers = MaxLobbyMembers,
+			Privacy = LobbyPrivacy.Private
+		};
+
+		//Networking.CreateLobby( config );
+	}
+
+	
+}
